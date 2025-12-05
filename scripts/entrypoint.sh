f714a6ca5803f7723f4f53d5fec592b7a225c6bf
Pierre Ozoux <pierre@ozoux.net>
2025-12-05 16:50:05 +0100

    fix(entrypoint): add retry
    In my test, the spilo container was slow to come up, and I didn't want to have a restart of this container.
    
    So here is the retry loop!

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-sleep 5
+for run in {1..30}; do
+  echo "waiting /run/etc/wal-e.d/env to come up... retry $run"
+  ls /run/etc/wal-e.d/env
+  if find /run/etc/wal-e.d/env -mindepth 1 -maxdepth 1 | read; then
+    echo "/run/etc/wal-e.d/env is populated, starting now..."
+    break
+  fi
+  sleep 6
+done
 exec envdir /run/etc/wal-e.d/env wal-g-prometheus-exporter
